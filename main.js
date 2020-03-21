c87f68743ac0eddfab2735c8e520030fdef2444b
Abhijeet Saxena <saxenaabhijeet@outlook.com>
2020-03-21 20:37:36 +0530

    feat(app): Add logic to play sound and animate

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-alert("Hello World");
+const sounds = document.querySelectorAll("audio");
+const pads = document.querySelector(".pads");
+const animation = document.querySelector(".ball-animation");
+
+pads.addEventListener(
+  "click",
+  event => {
+    const audio = event.target.children[0];
+    const targetColor = window
+      .getComputedStyle(event.target)
+      .getPropertyValue("background-color");
+
+    const ball = document.createElement("div");
+    ball.classList = "ball";
+    ball.style.background = targetColor;
+
+    document.body.appendChild(ball);
+
+    audio.currentTime = 0;
+    audio.play();
+  },
+  false
+);
