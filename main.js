351e9b60d37c3cc38c017a5a412db318b2ca6226
Abhijeet Saxena <saxenaabhijeet@outlook.com>
2020-03-28 16:35:22 +0530

    feat(client): Add ripple animation to header logo

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const pads = document.querySelector(".pads");
 
 const playSoundAndAnimate = target => {
   const audio = target.children[0];
+  audio.currentTime = 0;
+  audio.play();
   const color = window
     .getComputedStyle(target)
     .getPropertyValue("background-color");
@@ -12,9 +14,7 @@ const playSoundAndAnimate = target => {
   ball.style.animation = "bounce 1000ms ease-in-out";
   document.body.appendChild(ball);
   ball.onanimationend = () => document.body.removeChild(ball);
-
-  audio.currentTime = 0;
-  audio.play();
+  pulse();
 };
 
 pads.addEventListener("click", event => playSoundAndAnimate(event.target));
@@ -28,3 +28,29 @@ document.addEventListener(
   },
   false
 );
+
+const lines = document.querySelector(".lines");
+
+const linesArray = Array.from(document.querySelectorAll(".line"));
+
+window.onload = () => {
+  for (let i = 0; i < linesArray.length; i++) {
+    let targetHeight =
+      i === 2
+        ? "200"
+        : [0, 4, 9].indexOf(i) !== -1
+        ? "40"
+        : [6, 8].indexOf(i) !== -1
+        ? "65"
+        : "120";
+    if (window.innerWidth < 600) targetHeight *= 0.8;
+    linesArray[i].style.height = `${targetHeight}px`;
+  }
+};
+
+const pulse = () => {
+  lines.classList.add("ripple");
+  setTimeout(() => {
+    lines.classList.remove("ripple");
+  }, 600);
+};
