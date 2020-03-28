1b43fe6904f082807db5d55fd613074a072d8f11
Abhijeet Saxena <saxenaabhijeet@outlook.com>
2020-03-28 17:23:51 +0530

    test(app): See if permissions are working

--- main.js
+++ main.js
@@ -54,3 +54,52 @@ const pulse = () => {
     lines.classList.remove("ripple");
   }, 600);
 };
+
+let access = null;
+let op = document.querySelector(".op");
+let link = document.querySelector("a");
+
+const init = async () => {
+  access = await navigator.mediaDevices.getUserMedia({ audio: true });
+  onSuccess(access);
+};
+
+init();
+
+var constraints = { audio: true };
+var chunks = [];
+
+var onSuccess = function(stream) {
+  var options = {
+    mimeType: "audio/webm"
+  };
+
+  var mediaRecorder = new MediaRecorder(stream, options);
+
+  mediaRecorder.onstop = function(e) {
+    console.log("data available after MediaRecorder.stop() called.");
+
+    var audio = document.createElement("audio");
+    audio.controls = true;
+    var blob = new Blob(chunks, { type: "audio/mpeg; codecs=opus" });
+    var audioURL = window.URL.createObjectURL(blob);
+
+    link.href = audioURL;
+    audio.src = audioURL;
+    op.appendChild(audio);
+
+    console.log("recorder stopped");
+  };
+
+  mediaRecorder.ondataavailable = function(e) {
+    chunks.push(e.data);
+  };
+
+  mediaRecorder.start();
+  console.log("recorder started");
+
+  setTimeout(() => {
+    mediaRecorder.stop();
+    console.log("recorder stopped");
+  }, 20000);
+};
